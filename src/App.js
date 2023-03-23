c1e812e51b7f2a095ab6a4faa2a366dcba12b04d
hughesjh98 <89598311+hughesjh98@users.noreply.github.com>
2023-03-23 19:46:28 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello world
+          hello worl
         </p>
         <a
           className="App-link"
