f98c6fb39d352c96e67ca48bd63e29a98a66e466
hughesjh98 <89598311+hughesjh98@users.noreply.github.com>
2023-05-08 20:49:21 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import reportWebVitals from './reportWebVitals';
+// import * as atatus from 'atatus-spa';
+// atatus.config('4f3b660795244f6c906b7111cffbbbf3').install();
+// const root = document.getElementById("root");
+// render(<App />, root);
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+// //test if atatus has succesfully connected
+// // atatus.notify(new Error('Test Atatus Setup'));
+
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+//import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
-atatus.config('4f3b660795244f6c906b7111cffbbbf3').install();
-const root = document.getElementById("root");
-render(<App />, root);
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-//test if atatus has succesfully connected
-// atatus.notify(new Error('Test Atatus Setup'));
+atatus.config('4f3b660795244f6c906b7111cffbbbf3').install();
