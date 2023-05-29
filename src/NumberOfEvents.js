dbcc00cad985ee9fac382122b85bf7b40f825b70
hughesjh98 <89598311+hughesjh98@users.noreply.github.com>
2023-05-29 13:08:10 +0700

    update css

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -32,7 +32,7 @@ class NumberOfEvents extends Component {
             <div className="numberOfEvents">
                 <label>events displayed</label>
                 <input
-                    type="text"
+                    type="number"
                     className="input-number"
                     min={1}
                     max={32}
